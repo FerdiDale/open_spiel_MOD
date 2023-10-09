8a1d7040ed4a902b591326f6c1a66750ea2e9a62
Ferdinando <fer.dalessandro@studenti.unina.it>
2023-10-09 17:31:50 +0200

    modifiche alla build

--- open_spiel/BUILD.sh
+++ open_spiel/BUILD.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-BUILD_SHARED_LIB=ON CXX=clang++ cmake -DPython3_EXECUTABLE=$(which python3) -DCMAKE_CXX_COMPILER=${CXX} open_spiel
+mkdir build
+cd build
+
+BUILD_SHARED_LIB=ON CXX=clang++ cmake -DPython3_EXECUTABLE=$(which python3) -DCMAKE_CXX_COMPILER=${CXX} ../open_spiel
 
 make -j$(nproc) open_spiel
