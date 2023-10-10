3be0fde4ebd449123e3582e9bc30563f3c05f034
Ferdinando <fer.dalessandro@studenti.unina.it>
2023-10-10 11:10:49 +0200

    Modifiche allo script per compilare

--- open_spiel/open_spiel/examples/COMPILE.sh
+++ open_spiel/open_spiel/examples/COMPILE.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-g++ -g -I /home/ferdi/Desktop/Uni/OpenSpiel/open_spiel -I /home/ferdi/Desktop/Uni/OpenSpiel/open_spiel/open_spiel/abseil-cpp         -std=c++17 -o qlearn tabular_q_learning_example.cc         -L /home/ferdi/Desktop/Uni/OpenSpiel/open_spiel/build  -lopen_spiel -lboost_iostreams
+path_o_spiel_src="$(pwd)/../.."
+
+g++ -g -I "$path_o_spiel_src" -I "$path_o_spiel_src/open_spiel/abseil-cpp" -std=c++17 -o qlearn tabular_q_learning_example.cc  -L "$path_o_spiel_src/build"  -lopen_spiel -lboost_iostreams
+
+/bin/bash
