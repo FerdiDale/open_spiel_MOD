5d4803ef9e7228727b731da84d94412eba8f1122
Ferdinando <fer.dalessandro@studenti.unina.it>
2023-10-10 11:23:56 +0200

    altra prova

--- open_spiel/open_spiel/examples/COMPILE.sh
+++ open_spiel/open_spiel/examples/COMPILE.sh
@@ -7,5 +7,3 @@ LD_LIBRARY_PATH="$path_o_spiel_src/build"
 export LD_LIBRARY_PATH
 
 g++ -g -I "$path_o_spiel_src" -I "$path_o_spiel_src/open_spiel/abseil-cpp" -std=c++17 -o qlearn tabular_q_learning_example.cc  -L "$path_o_spiel_src/build"  -lopen_spiel -lboost_iostreams
-
-/bin/bash
